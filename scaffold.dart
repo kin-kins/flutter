abcfecdedb52a603feee890792048a749ddd92ea
Ashu Kumar <30663362+kin-kins@users.noreply.github.com>
2018-11-06 03:55:41 +0530

    Update scaffold.dart

--- scaffold.dart
+++ scaffold.dart
@@ -5,20 +5,33 @@ class scaffoldHome extends StatelessWidget
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    void onPress()
-    {
+    void onPress() {
       print("tapped");
     }
     return (
         new Scaffold(
             appBar: new AppBar(
 
-              title: new Text("Scaffold",textAlign: TextAlign.left, textDirection: TextDirection.ltr,),
+              title: new Text("Scaffold", textAlign: TextAlign.left,
+                textDirection: TextDirection.ltr,),
               backgroundColor: Colors.pinkAccent,
               actions: <Widget>[
-                new IconButton(icon: new Icon(Icons.send), onPressed: ()=> debugPrint("send tapped!")),
+                new IconButton(icon: new Icon(Icons.send),
+                    onPressed: () => debugPrint("send tapped!")),
                 new IconButton(icon: new Icon(Icons.search), onPressed: onPress)
               ],
+            ),
+            body: new Center(
+                child: new Container(
+                    child: new InkWell(
+                      child: new Text("Button",
+                      style:new TextStyle(fontSize: 28.0)),
+                      onTap: () => debugPrint("Button Pressed"),
+                      //highlightColor: Colors.pinkAccent,
+                      splashColor: Colors.yellowAccent,
+
+                    )
+                )
             )
         )
     );
